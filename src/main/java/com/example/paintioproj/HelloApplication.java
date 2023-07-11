cdd4404f2e8849c3e8f5908670940ff2a198a57d
Helia <thehellwin@gmail.com>
2023-07-11 10:37:11 +0330

    game pane, icon, title made

--- src/main/java/com/example/paintioproj/HelloApplication.java
+++ src/main/java/com/example/paintioproj/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.paintioproj;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Image icon = new Image("F:\\paint-io-proj\\src\\icon.png");
+        Scene scene = new Scene(fxmlLoader.load(), 650, 650);
+        stage.setTitle("Paint.IO");
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
